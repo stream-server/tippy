00fac2817a83ed9d25b6e07e75a0e80adef5f5dd
hotcocoavideostreaming <hotcocoavideostreaming@gmail.com>
2024-12-24 19:46:35 +0900

    test

---
+++ test/video.js
@@ -0,0 +1,31 @@
+        const spans = document.querySelectorAll('span');
+        const iframeContainer = document.getElementById('iframe-container');
+        const resultDiv = document.getElementById('main-video-title');
+        let currentIframe;
+
+        spans.forEach(span => {
+            span.addEventListener('click', () => {
+                const iframeUrl = span.dataset.iframeUrl;
+                const text = span.dataset.text;
+
+                // 기존 iframe 제거
+                if (currentIframe) {
+                    iframeContainer.removeChild(currentIframe);
+                }
+
+                // 새로운 iframe 생성 및 추가
+                if (iframeUrl) {
+                    const newIframe = document.createElement('iframe');
+                    newIframe.src = iframeUrl;
+                    newIframe.allowFullscreen = true;
+                    iframeContainer.classList.add('iframe-container');
+                    iframeContainer.appendChild(newIframe);
+                    currentIframe = newIframe;
+                }
+
+                // 결과 div에 텍스트 출력
+                if (text) {
+                    resultDiv.textContent = text;
+                }
+            });
+        });
