311b32f00903f8babafc8739bf9d5f14b40f19e7
hotcocoavideostreaming <hotcocoavideostreaming@gmail.com>
2025-01-18 14:43:22 +0900

    js

--- js/viewport-video.js
+++ js/viewport-video.js
@@ -20,28 +20,32 @@ for (const span of spans) {
       newIframe.src = iframeUrl;
       newIframe.allowFullscreen = true;
 
-      // iframe에 전체 화면 이벤트 리스너 추가
+      // iframe에 전체 화면 이벤트 리스너 추가 (한 번만 추가)
       newIframe.addEventListener('click', function() {
         (newIframe.requestFullscreen || newIframe.mozRequestFullScreen || newIframe.webkitRequestFullscreen || newIframe.msRequestFullscreen).call(newIframe);
 
-        screen.orientation.lock('landscape').then(function() {
-          console.log('화면이 가로 방향으로 고정되었습니다.');
-        }).catch(function(error) {
-          console.log('화면 고정에 실패했습니다.: ', error);
-        });
+        // 브라우저 호환성 확인
+        if (screen.orientation && screen.orientation.lock) {
+          screen.orientation.lock('landscape').then(function() {
+            console.log('화면이 가로 방향으로 고정되었습니다.');
+          }).catch(function(error) {
+            console.log('화면 고정에 실패했습니다.: ', error);
+          });
+        }
       });
 
       newIframe.addEventListener('fullscreenchange', function() {
         if (!document.fullscreenElement) {
           (document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen).call(document);
 
-          screen.orientation.unlock();
+          if (screen.orientation && screen.orientation.unlock) {
+            screen.orientation.unlock();
+          }
           console.log('화면 회전이 해제되었습니다.');
         }
       });
 
-      VideoMain.classList.add('video-main');
-      VideoMain.appendChild(newIframe);
+      VideoMain.appendChild(newIframe); 
     }
 
     // 결과 div에 텍스트 출력
@@ -49,3 +53,16 @@ for (const span of spans) {
     MainVideoSubtitle.textContent = subtitle;
   });
 }
+
+// 화면 방향 변화 감지
+window.addEventListener("orientationchange", function() {
+  // video-main 내부의 iframe 요소 가져오기
+  var iframe = document.querySelector("#video-main iframe"); 
+
+  // 화면 방향에 따라 iframe 회전
+  if (window.orientation == 90 || window.orientation == -90) {
+    iframe.style.transform = "rotate(90deg)"; 
+  } else {
+    iframe.style.transform = "rotate(0deg)"; 
+  }
+});
